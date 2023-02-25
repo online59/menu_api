1129e7409ec5ba7c51214072d1eede69697159f0
online59 <taez.khamrangsi@gmail.com>
2023-02-25 08:03:37 +0700

    Add new project

---
+++ src/main/java/ku/menu/entity/Menu.java
@@ -0,0 +1,23 @@
+package ku.menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Data
+@ToString
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private double price;
+    private String category;
+}
---
+++ src/main/java/ku/menu/service/MenuService.java
@@ -0,0 +1,69 @@
+package ku.menu.service;
+
+import ku.menu.entity.Menu;
+import ku.menu.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class MenuService {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    public List<Menu> getAll() {
+        return menuRepository.findAll();
+    }
+
+    public Menu getById(UUID id) {
+
+        if (menuRepository.findById(id).isPresent()) {
+            return menuRepository.findById(id).get();
+        } else return null;
+
+    }
+
+    public Menu getMenuByName(String name) {
+        return menuRepository.findByName(name);
+    }
+
+    public List<Menu> getMenuByCategory(String category) {
+        return menuRepository.findByCategory(category);
+    }
+
+    public Menu create(Menu menu) {
+        System.out.println("Before JPA: " + menu);
+        Menu record = menuRepository.save(menu);
+        System.out.println("After JPA: " + menu);
+        return record;
+    }
+
+    public Menu update(Menu requestBody) {
+        UUID id = requestBody.getId();
+        if (menuRepository.findById(id).isPresent()) {
+            Menu record = menuRepository.findById(id).get();
+            record.setName(requestBody.getName());
+            record.setPrice(requestBody.getPrice());
+            record.setCategory(requestBody.getCategory());
+
+            return menuRepository.save(record);
+        } else {
+            return null;
+        }
+    }
+
+    public Menu delete(UUID id) {
+        if (menuRepository.findById(id).isPresent()) {
+            Menu record = menuRepository.findById(id).get();
+            menuRepository.deleteById(id);
+            return record;
+        } else {
+            return null;
+        }
+    }
+}
